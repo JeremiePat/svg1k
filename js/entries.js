3bf0aa181c06dd1c5cb12c72b426c5c0b2685d56
Jérémie Patonnier <jeremie.patonnier@gmail.com>
2020-06-12 21:56:19 +0200

    Animated clouds

--- js/entries.js
+++ js/entries.js
@@ -3,5 +3,6 @@ export default [
   // of the author and a link. The author name and the link are optional but
   // are an easy way to give back credits for the contribution. The link can be
   // either a point of contact or a link to a detail explanation of the demo.
+  { file: 'clouds.svg', author: "Jérémie Patonnier", link: "https://twitter.com/JeremiePat/" },
   { file: 'svg1k.svg', author: "Jérémie Patonnier", link: "https://twitter.com/JeremiePat/" },
 ]
