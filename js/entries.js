5d752914247fae4beffc3609ce068155fb13fa62
Thomas Sauques <39383180+Morphyish@users.noreply.github.com>
2020-06-17 17:52:35 +0200

    boil and bubbles entry (#4)

--- js/entries.js
+++ js/entries.js
@@ -7,4 +7,5 @@ export default [
   { file: 'svg1k.svg', author: "Jérémie Patonnier", link: "https://twitter.com/JeremiePat/" },
   { file: 'css-keyframes-spinner.svg', author: "Amelia Bellamy-Royds", link: "https://codepen.io/AmeliaBR/" },
   { file: 'loading-squares.svg', author: "Thomas Sauques", link: "https://github.com/Morphyish" },
+  { file: 'boil-and-bubbles.svg', author: "Thomas Sauques", link: "https://github.com/Morphyish" },
 ]
